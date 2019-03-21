a6c1c0b996fdd8e1246cdb8116291fe3ad9cb831
Ruben De Man <ruben.deman@gmail.com>
2019-03-21 12:16:29 -0400

    Update thresh_analysis.m

--- thresh_analysis.m
+++ thresh_analysis.m
@@ -1,41 +1,41 @@
-% RDM 080218
+% RDM Created 080218, Revised 032119
 
-thresh=600; % Must specify threshold
-sliceno=1;
+thresh=0; %Must specify threshold
+sliceno=10;
 checksize=5;
-filename={'1','2','3','4','5','6','7','8','9','10','11','12','13','14'};
+expfiles={};
+contfiles={};
 
-for j=1:length(filename)
-a=regread(strcat(filename{j},'_registered.nii'),sliceno);
-b=regread(strcat(filename{j},'_processed.nii'),sliceno);
-orig{j}=b;
-reg{j}=a;
-thold{j}=reg{j};
-thold{j}(thold{j}<thresh)=0;
+for j=1:length(expfiles)
+a=avgread(strcat(expfiles{j},'.nii'));
+b=avgread(strcat(contfiles{j},'.nii'));
+expimage{j}=a(:,:,sliceno);
+expimage{j}(expimage{j}<thresh)=0;
+contimage{j}=b(:,:,sliceno);
+contimage{j}(contimage{j}<thresh)=0;
 end
 
 figure
-imshow([orig{2},orig{3},orig{5},orig{6};orig{7},orig{8},orig{9},orig{10};orig{11},orig{12},orig{13},orig{14}],[]);
+imshow([expimage{1}],[])
 figure
-imshow([reg{2},reg{3},reg{5},reg{6};reg{7},reg{8},reg{9},reg{10};reg{11},reg{12},reg{13},reg{14}],[]);
-figure
-imshow([thold{2},thold{3},thold{5},thold{6};thold{7},thold{8},thold{9},thold{10};thold{11},thold{12},thold{13},thold{14}],[]);
+expdisp=cat(1,expimage{:});
+contdisp=cat(1,contimage{:});
+imshow([expdisp;contdisp],[]);
+exp=mean(cat(3,expimage{:}), 3);
+cont=mean(cat(3,contimage{:}), 3);
 
-drug=(thold{3}+thold{5}+thold{7}+thold{9}+thold{11}+thold{13})/6;
-cont=(thold{2}+thold{6}+thold{8}+thold{10}+thold{12}+thold{14})/6;
-
 figure
-imshow([drug,cont],[])
-diff=drug-cont;
-% figure
-% imshow([diff],[])
+imshow([exp,cont],[])
+diff=exp-cont;
+figure
+imshow([diff],[])
 figure
 imagesc(diff)
 colormap(hot)
 colorbar
 
 [y,x]=ginput(1);
-drugcheck=drug(x-checksize:x+checksize,y-checksize:y+checksize);
+expcheck=exp(x-checksize:x+checksize,y-checksize:y+checksize);
 contcheck=cont(x-checksize:x+checksize,y-checksize:y+checksize);
 
-[h,p,ci,stats] = ttest2(reshape(drugcheck,[1,numel(drugcheck)]),reshape(contcheck,[1,numel(contcheck)]))
+[h,p,ci,stats] = ttest2(reshape(expcheck,[1,numel(expcheck)]),reshape(contcheck,[1,numel(contcheck)]))
