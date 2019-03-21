2faaedbe111e0c77dc16943fe0b092daa7da902b
Ruben De Man <ruben.deman@gmail.com>
2019-03-21 12:14:45 -0400

    Delete avgtime4reg.m

--- avgtime4reg.m
+++
@@ -1,9 +0,0 @@
-% RDM 080218
-
-filename={'1','2','3','4','5','6','7','8','9','10','11','12','13','14','reference'};
-
-for i=1:length(filename)
-    V=niftiread(strcat(filename{i},'.nii'));
-    W=mean(V,4);
-    niftiwrite(W,strcat(filename{i},'_processed.nii'));
-end
